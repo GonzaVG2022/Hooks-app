8be2db16196add97157aa37ef34e145b4c8c92c7
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-08-11 09:13:04 -0300

    inicio Rick and Morty

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { CounterWhitCustomHook } from './01-useState/CounterWhitCustomHook'
 import './style.css'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { CounterWhitCustomHook } from './01-useState/CounterWhitCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { Counter } from './Counter'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <FormWithCustomHook/>
+  // <FormWithCustomHook/>
+  <MultipleCustomHooks/>
     // <SimpleForm/>
     // <CounterWhitCustomHook/> 
   //  <Counter/>
