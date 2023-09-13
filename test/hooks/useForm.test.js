7deca4b635e740167a443506be221ae30ee86c3b
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-09-13 20:57:30 -0300

    test-useForm

---
+++ test/hooks/useForm.test.js
@@ -0,0 +1,61 @@
+import { act, renderHook } from "@testing-library/react"
+import { useForm } from "../../src/hooks/useForm"
+
+describe('Pruebas en el useForm', () => { 
+    const initialForm = {
+        name: 'Flavio',
+        email: 'gonzagargantini20@gmail.com'
+    }
+    test('debe regresar valores por defecto', () => { 
+        const { result } = renderHook( () => useForm(initialForm) );
+        // console.log(result); 
+        
+        expect(result.current).toEqual(
+            {
+                
+                  name: initialForm.name,
+                  email: initialForm.email,
+                  formState: initialForm,
+                  onInputChange: expect.any( Function ),
+                  onResetForm: expect.any( Function )
+               
+            }
+        );
+    
+    });
+    test('debe de cambiar el nombre del formulario', () => { 
+        const newValue = 'Flavio';
+        const { result } = renderHook( () => useForm(initialForm) );
+        const { onInputChange } = result.current;
+        act( () => {
+            onInputChange({
+                target: {
+                    name: 'name',
+                    value: newValue
+                }
+            });
+            expect( result.current.name ).toBe( newValue );
+            expect( result.current.formState.name ).toBe( newValue );  
+        } )
+     });
+
+     test('debe de realizar el reset del formulario', () => { 
+        const newValue = 'Gonzalo';
+        const { result } = renderHook( () => useForm(initialForm) );
+        const { onInputChange, onResetForm } = result.current;
+        act( () => {
+             onInputChange({
+                target: {
+                    name: 'name',
+                    value: newValue
+                }            
+            });
+             onResetForm();
+        } );
+        expect( result.current.name ).toBe( initialForm.name );
+        expect( result.current.formState.name ).toBe( initialForm.name ); 
+     });
+
+
+
+ });
